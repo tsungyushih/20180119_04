5b890ec43e389b0de69ce2f87e4828204055f9f1
Frank <Frank@hotmail.com>
2018-01-19 14:18:23 +0800

    Initial commit

---
+++ app/src/main/java/com/frandog/a20180119_04/MainActivity.java
@@ -0,0 +1,29 @@
+package com.frandog.a20180119_04;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.layout1,new FragmentA());
+        ft.commit();
+    }
+    public void click2(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.layout1,new FragmentB());
+        ft.commit();
+    }
+}
